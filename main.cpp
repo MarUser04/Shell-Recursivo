1e312a3d234fa0ff707455697b36d4f2f3701357
Mario Grieco <mariogrieco@users.noreply.github.com>
2017-02-17 22:55:04 -0400

    Mejorado el orden.
    nada nuevo ....

--- main.cpp
+++ main.cpp
@@ -4,41 +4,51 @@
 #include <time.h>
 using namespace std;
 
-void imprimir(int vec[], int n);
-void Shell(int vec[], int n, int salto);
+void imprimir(int[], int);
+void Shell(int[], int, int);
+void initRand(void);
+void generate(int[],int);
+   
+   clock_t inicio,fin;
 
 int main(int argc, char *argv[])
 {
-   clock_t inicio,fin;
-
-
     int n=500000;
-     int vec[n];
+    int vec[n];
 
-  /*  for(int i=0; i<n; i++){
+  /*  
+     for(int i=0; i<n; i++){
        cout<<"Ingrese numero: ";
         cin>>vec[i];
-    }*/
+     }
+  */
+     initRand();
+     generate(vec,n);
 
-     srand(time(NULL));
-
-     for(int i=0; i<n; i++){
-            //vec[i]=rand()%(50-1)+1;
-          vec[i]=rand()%(500000-1)+1;
-         }
-
-      int salto=n/2;
+    int salto=n/2;
      //imprimir(vec, n);
      cout<<endl<<endl;
-         inicio = clock();
-     Shell(vec, n, salto);
-    //imprimir(vec, n);
+    inicio = clock();
+    Shell(vec, n, salto);
+    // imprimir(vec, n);
 
 
 
-       cout<<endl<<"Tiempo de ejecucion Shell: "<<(((double)clock() - inicio) / CLOCKS_PER_SEC)*1000<< " milisegundos"<<endl;
+     cout<<endl<<"Tiempo de ejecucion Shell: "<<( (double)(fin - inicio) / (double)CLOCKS_PER_SEC ) *1000 << " milisegundos"<<endl;
      return 0;
  }
+
+void initRand()
+{
+   srand(time(NULL));
+}
+
+void generate(int v[],int length){
+     for(int i=0; i<length; i++){
+          //vec[i]=rand()%(50-1)+1;
+         v[i]=rand()%(length-1)+1;  
+     }
+}
 
 void imprimir(int vec[], int n){
     for(int i=0; i<n; i++){
@@ -47,9 +57,7 @@ void imprimir(int vec[], int n){
 }
 
 void Shell(int vec[], int n, int salto){
-
   int aux;
-
     bool band=true;
 
 
@@ -73,4 +81,5 @@ void Shell(int vec[], int n, int salto){
         if(salto!=0)
             Shell(vec, n, salto);
 
+   fin = clock();
 }
